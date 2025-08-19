6af10ec6b035e8efa853da9972993acc15619f73
amirjanshekar <a.janshekar@yahoo.com>
2025-08-19 15:57:26 +0330

    feat: adding ordering

--- src/_core/settings/base.py
+++ src/_core/settings/base.py
@@ -128,6 +128,7 @@ REST_FRAMEWORK = {
         ),
     "DEFAULT_FILTER_BACKENDS": (
         "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.OrderingFilter"
     ),
     "DEFAULT_PAGINATION_CLASS": "_core.pagination.PageNumberPagination",
     "PAGE_SIZE": 20,
